2347b1390683493d5fc3e1a18907c742e5eacf53
Rinat <hicc0rus@gmail.com>
2024-05-30 14:58:38 +0500

    added stream

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,9 @@
 
-export * from './crypto';
+import { Handler } from './handler';
+import { ZilPay } from './zilpay';
+
+export const handler = Object.freeze(new Handler());
+export const zilPay = Object.freeze(new ZilPay(handler.stream, handler.subject));
+handler.initialized();
+window['zilPay'] = zilPay;
 
---
+++ lib/stream/stream-keys.ts
@@ -0,0 +1,33 @@
+const app = 'zil-pay';
+
+export const MTypeTabContent = {
+  CONTENT: `@/${app}/content-script`,
+  INJECTED: `@/${app}/injected-script`
+};
+
+export const MTypeTab = {
+  GET_WALLET_DATA: `@/${app}/injected-get-wallet-data`,
+  ADDRESS_CHANGED: `@/${app}/address-changed`,
+  NETWORK_CHANGED: `@/${app}/network-changed`,
+  LOCK_STAUS: `@/${app}/lack-status-updated`,
+
+  CONTENT_PROXY_MEHTOD: `@/${app}/request-through-content`,
+  CONTENT_PROXY_RESULT: `@/${app}/response-from-content`,
+
+  CALL_TO_SIGN_TX: `@/${app}/request-to-sign-tx`,
+  TX_RESULT: `@/${app}/response-tx-result`,
+
+  SIGN_MESSAGE: `@/${app}/request-to-sign-message`,
+  SING_MESSAGE_RES: `@/${app}/response-sign-message`,
+
+  NEW_BLOCK: `@/${app}/new-block-created`,
+
+  ADD_ENCRYPTION: `@/${app}/request-add-encryption`,
+  RES_ENCRYPTION: `@/${app}/response-encryption`,
+  ADD_DECRYPTION: `@/${app}/add-decryption`,
+  RES_DECRYPTION: `@/${app}/response-decryption`,
+
+  CONNECT_APP: `@/${app}/request-to-connect-dapp`,
+  RESPONSE_TO_DAPP: `@/${app}/response-dapp-connect`,
+  DISCONNECT_APP: `@/${app}/request-to-disconnect-dapp`
+};
