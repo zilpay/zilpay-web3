93c9b4c67672654cbdea2c904fda8c91c5cccf64
Rinat <hicc0rus@gmail.com>
2024-05-30 12:20:44 +0500

    added crypto files

---
+++ lib/crypto/validator.ts
@@ -0,0 +1,19 @@
+import BN from 'bn.js';
+import Long from 'long';
+import { isBech32, isByteString } from 'lib/utils/address/hex';
+
+export const Validator = {
+  isAddress(x: unknown) {
+    return isByteString(String(x), 40);
+  },
+  isBech32(x: unknown) {
+    return isBech32(String(x));
+  },
+  isBN(x: unknown) {
+    return BN.isBN(x);
+  },
+  isLong(x: unknown) {
+    return Long.isLong(x);
+  }
+};
+
