84f08b63521807fc229ce3bd406ece162757bd7b
Rinat <rinat@Rinats-MacBook-Air.local>
2025-02-21 12:54:24 +0900

    fixed build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,29 +1,43 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default [
-	{
-		input: 'index.ts',
-		output: {
-			dir: 'dist',
-			name: 'web3',
-			format: production ? 'es' : 'umd',
-			sourcemap: true
-		},
-		plugins: [
-			resolve({
-				brower: true,
-				preferBuiltins: false,
-			}),
-			typescript({
-				sourceMap: true,
-				declaration: true,
-				inlineSources: true
-			}),
-			commonjs()
-		]
-	}
+  {
+    input: 'index.ts',
+    output: [
+      {
+        dir: 'dist',
+        name: 'web3',
+        format: production ? 'es' : 'umd',
+        sourcemap: true
+      },
+      {
+        file: 'dist/inpage.js',
+        format: 'iife',
+        name: 'zilPayWeb3',
+        sourcemap: true,
+        plugins: [terser({
+          compress: {
+            drop_console: production, 
+            pure_funcs: ['console.log'], 
+            passes: 3 
+          },
+          mangle: true, 
+          output: {
+            comments: false 
+          }
+        })]
+      }
+    ],
+    plugins: [
+      resolve({ browser: true, preferBuiltins: false }),
+      typescript({ sourceMap: true, declaration: true, inlineSources: true }),
+      commonjs(),
+      production && terser() 
+    ]
+  }
 ];
